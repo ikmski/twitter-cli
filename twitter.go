2a7bfbe6ece222c644151327840e83bd66ab20f9
masaki <ikeda.masaki@gmail.com>
2018-03-25 17:44:10 +0900

    more simple

--- twitter.go
+++ twitter.go
@@ -1,71 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"net/url"
 
 	"github.com/ChimeraCoder/anaconda"
 )
 
 type twitter struct {
-	screen *screen
-	buffer *buffer
-
-	updateCh chan bool
 }
 
 func newTwitter() *twitter {
 
 	t := new(twitter)
 
-	t.updateCh = make(chan bool)
-
-	t.screen = newScreen()
-	t.buffer = newBuffer(10)
-
 	return t
 }
 
 func (t *twitter) userStream() {
 
-	go func() {
+	api := anaconda.NewTwitterApiWithCredentials(
+		config.AccessToken,
+		config.AccessTokenSecret,
+		config.ConsumerKey,
+		config.ConsumerSecret)
 
-		api := anaconda.NewTwitterApiWithCredentials(
-			config.AccessToken,
-			config.AccessTokenSecret,
-			config.ConsumerKey,
-			config.ConsumerSecret)
-
-		list, _ := api.GetHomeTimeline(url.Values{})
-		for i := len(list) - 1; i >= 0; i-- {
-			t.buffer.push(list[i])
-		}
-		t.updateCh <- true
-
-		stream := api.UserStream(url.Values{})
-
-		for tweet := range stream.C {
-
-			switch content := tweet.(type) {
-
-			case anaconda.Tweet:
-
-				t.buffer.push(content)
-				t.updateCh <- true
-
-			}
-
-		}
-
-	}()
-
-	for {
-
-		select {
-
-		case <-t.updateCh:
-			t.screen.render(t.buffer.toList())
-
-		}
+	tweets, _ := api.GetHomeTimeline(url.Values{})
+	for i := len(tweets) - 1; i >= 0; i-- {
+		output(tweets[i])
 	}
+
+	stream := api.UserStream(url.Values{})
+
+	for tweet := range stream.C {
+
+		switch content := tweet.(type) {
+
+		case anaconda.Tweet:
+
+			output(content)
+		}
+
+	}
+
+}
+
+func output(t anaconda.Tweet) {
+
+	fmt.Printf("%s %s (%s)\n", t.User.Name, t.User.ScreenName, t.CreatedAt)
+	fmt.Printf("%s\n", t.FullText)
+	fmt.Printf("%s\n\n", "------------------------------------------------")
 
 }
